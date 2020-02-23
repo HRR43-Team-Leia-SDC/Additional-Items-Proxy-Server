b031774d1d3dfaa8a4014b5c4193d9f74e6ca3ca
yerx <yer.x.xiong@gmail.com>
2020-02-23 14:01:23 -0500

    setting up proxy for service

--- ProxyServer/server/index.js
+++ ProxyServer/server/index.js
@@ -3,42 +3,51 @@ const app = express();
 const axios = require('axios');
 
 require('dotenv').config();
-const port = process.env.PROXY_PORT;
+const port = process.env.PROXY_PORT || 3005;
 app.use(express.urlencoded({ extended: true }));
 
 app.use(express.static('./public'));
 
-app.get('/carousel/:id', (req, res) => {
-  const id = req.params.id
-  // additional items
-  axios.get(`http://localhost:3001/carousel/${id}`)
-    .then((response) => {
-      res.status(200).json(response.data);
-    });
-});
+// app.get('/carousel/:id', (req, res) => {
+//   const id = req.params.id
+//   // additional items
+//   axios.get(`http://localhost:3001/carousel/${id}`)
+//     .then((response) => {
+//       res.status(200).json(response.data);
+//     });
+// });
 
-app.get('/details/:id', (req, res) => {
-  const id = req.params.id
-  // additional items
-  axios.get(`http://ec2-18-220-152-98.us-east-2.compute.amazonaws.com/details/${id}`)
-    .then((response) => {
-      res.status(200).json(response.data);
-    });
-});
+// app.get('/details/:id', (req, res) => {
+//   const id = req.params.id
+//   // additional items
+//   axios.get(`http://ec2-18-220-152-98.us-east-2.compute.amazonaws.com/details/${id}`)
+//     .then((response) => {
+//       res.status(200).json(response.data);
+//     });
+// });
 
-app.get('/reviews/:id', (req, res) => {
-  const id = req.params.id
-  // additional items
-  axios.get(`http://ec2-3-14-246-138.us-east-2.compute.amazonaws.com/reviews/${id}`)
-    .then((response) => {
-      res.status(200).json(response.data);
-    });
-});
+// app.get('/reviews/:id', (req, res) => {
+//   const id = req.params.id
+//   // additional items
+//   axios.get(`http://ec2-3-14-246-138.us-east-2.compute.amazonaws.com/reviews/${id}`)
+//     .then((response) => {
+//       res.status(200).json(response.data);
+//     });
+// });
+
+// app.get('/additional/:id', (req, res) => {
+//   const id = req.params.id
+//   // additional items
+//   axios.get(`http://ec2-3-133-117-113.us-east-2.compute.amazonaws.com/additional/${id}`)
+//     .then((response) => {
+//       res.status(200).json(response.data);
+//     });
+// });
 
 app.get('/additional/:id', (req, res) => {
   const id = req.params.id
   // additional items
-  axios.get(`http://ec2-3-133-117-113.us-east-2.compute.amazonaws.com/additional/${id}`)
+  axios.get(`http://localhost:3004/additional/${id}`)
     .then((response) => {
       res.status(200).json(response.data);
     });
